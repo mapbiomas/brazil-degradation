483b1e544cd5c5e55aefe55c1d9085aff0e24513
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2025-08-14 09:24:29 -0300

    Update 03_fragmentation_isolation_stepC.js

--- 03_fragmentation_isolation_stepC.js
+++ 03_fragmentation_isolation_stepC.js
@@ -1,4 +1,4 @@
-var v_out = '4'
+var v_out = '1'
 
 var Palettes = require('users/mapbiomas/modules:Palettes.js');
 var palette = Palettes.get('classification8');
@@ -19,14 +19,14 @@ Map.addLayer(bioma250mil, {}, 'Bioma Raster', false)
 
 //var forestMap = ee.Image('projects/mapbiomas-workspace/DEGRADACAO/ISOLATION/BR_forestMask_conn')
 //var forestMap = ee.Image('projects/mapbiomas-workspace/DEGRADACAO/ISOLATION/nonforestMask85a23_100m_v2_conn')
-var nonforestMap = ee.Image('projects/mapbiomas-workspace/DEGRADACAO/ISOLATION_col9_v2/natural_Mask85a23_100m_v3_conn')
+var nonforestMap = ee.Image('projects/mapbiomas-workspace/DEGRADACAO/ISOLATION_col10_v1/natural_Mask85a24_100m_v1_conn')
 //print(forestMap)
 print(nonforestMap)
 
 var years = ['1985','1986','1987','1988','1989','1990','1991','1992','1993','1994',
              '1995','1996','1997','1998','1999','2000','2001','2002','2003','2004',
              '2005','2006','2007','2008','2009','2010','2011','2012','2013','2014',
-             '2015','2016','2017','2018','2019','2020','2021','2022','2023'];
+             '2015','2016','2017','2018','2019','2020','2021','2022','2023', '2024'];
 //var years = ['2022'];
 
 for (var i_year=0;i_year<years.length; i_year++){
@@ -64,7 +64,7 @@ for (var i_year=0;i_year<years.length; i_year++){
 //  Map.addLayer(distanceTonatural_maior500ha_ano, {  min: 0,  max: 25000,palette: ['yellow','red','#3d1817']}, 'distanceTonatural_maior500ha_ano', true)
 //  Map.addLayer(distanceTonatural_maior100ha_ano, {  min: 0,  max: 25000,palette: ['yellow','red','#3d1817']}, 'distanceTonatural_maior100ha_ano', true)
 
-  var dirout = 'projects/mapbiomas-workspace/DEGRADACAO/ISOLATION_col9_v2/BR_Distance/'
+  var dirout = 'projects/mapbiomas-workspace/DEGRADACAO/ISOLATION_col10_v1/BR_Distance/'
   
   // Get a list of forest fragments using connectedComponents().
   var naturalFragments_menor1000ha_ano = con_menor_1000ha_ano.connectedComponents({connectedness: ee.Kernel.plus(1),maxSize: 1001});
@@ -99,45 +99,45 @@ print(distanceTonatural_maior1000ha)
 
   Export.image.toAsset({
    "image": distanceTonatural_maior1000ha.toInt16(),
-   "description": 'distanceToforestMask_maior1000ha_85_23'+v_out,
-   "assetId": dirout + 'distanceToforestMask_maior1000ha_85_23'+v_out,
+   "description": 'distanceToforestMask_maior1000ha_85_24'+v_out,
+   "assetId": dirout + 'distanceToforestMask_maior1000ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
   Export.image.toAsset({
    "image": distanceTonatural_maior500ha.toInt16(),
-   "description": 'distanceToforestMask_maior500ha_85_23'+v_out,
-   "assetId": dirout + 'distanceToforestMask_maior500ha_85_23'+v_out,
+   "description": 'distanceToforestMask_maior500ha_85_24'+v_out,
+   "assetId": dirout + 'distanceToforestMask_maior500ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
   Export.image.toAsset({
    "image": distanceTonatural_maior100ha.toInt16(),
-   "description": 'distanceToforestMask_maior100ha_85_23'+v_out,
-   "assetId": dirout + 'distanceToforestMask_maior100ha_85_23'+v_out,
+   "description": 'distanceToforestMask_maior100ha_85_24'+v_out,
+   "assetId": dirout + 'distanceToforestMask_maior100ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
 
   Export.image.toAsset({
    "image": naturalFragments_menor1000ha,
-   "description": 'naturalFragments_menor1000ha_85_23'+v_out,
-   "assetId": dirout + 'naturalFragments_menor1000ha_85_23'+v_out,
+   "description": 'naturalFragments_menor1000ha_85_24'+v_out,
+   "assetId": dirout + 'naturalFragments_menor1000ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
   
     Export.image.toAsset({
    "image": natural_mask_maior1000ha.toInt16(),
-   "description": 'natural_mask_maior1000ha_85_23'+v_out,
-   "assetId": dirout + 'natural_mask_maior1000ha_85_23'+v_out,
+   "description": 'natural_mask_maior1000ha_85_24'+v_out,
+   "assetId": dirout + 'natural_mask_maior1000ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
   Export.image.toAsset({
    "image": natural_mask_maior500ha.toInt16(),
-   "description": 'natural_mask_maior500ha_85_23'+v_out,
-   "assetId": dirout + 'natural_mask_maior500ha_85_23'+v_out,
+   "description": 'natural_mask_maior500ha_85_24'+v_out,
+   "assetId": dirout + 'natural_mask_maior500ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
   Export.image.toAsset({
    "image": natural_mask_maior100ha.toInt16(),
-   "description": 'natural_mask_maior100ha_85_23'+v_out,
-   "assetId": dirout + 'natural_mask_maior100ha_85_23'+v_out,
+   "description": 'natural_mask_maior100ha_85_24'+v_out,
+   "assetId": dirout + 'natural_mask_maior100ha_85_24'+v_out,
    "scale": 100, "pyramidingPolicy": {'.default': 'mode'},"maxPixels": 1e13, "region": geometry
   });  
