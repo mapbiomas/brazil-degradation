00570928dfdb7d6eb4af6464827cd1551a086e52
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2026-02-13 09:55:55 -0300

    Update 01b_fragmentation_edgeAge_v2.js

--- 01b_fragmentation_edgeAge_v2.js
+++ 01b_fragmentation_edgeAge_v2.js
@@ -6,6 +6,9 @@ var version_in = 1;
 var version_out = 1;
 var collectionId = 10;
 
+// set filename
+var fileName = 'degradation_edge_age_col' + collectionId + '_v' + version_out;
+
 // set years to be processed
 var years = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
              1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
@@ -15,13 +18,8 @@ var years = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
 
   
 // read edges for the version i and binarize 
-var edge_i_bin = ee.ImageCollection('projects/mapbiomas-brazil/assets/DEGRADATION/COLLECTION-10/edge-area')
-  .filterMetadata('version', 'equals',  version_in)     // get version 
-  .map(function(image) {                             // function to binarize edges
-    return image.gte(1).set({'year': image.get('year')
-    });
-  });
-
+var edge_i_bin = ee.Image('projects/mapbiomas-brazil/assets/DEGRADATION/COLLECTION-10/public/degradation_edge_area_col10_v1')
+  .gte(1); // binarize
 
 // for each year
 var ages_i = ee.Image([]);
@@ -30,24 +28,24 @@ years.forEach(function(year_j) {
   // if years is the first, maintain input as they are
   if (year_j == 1985) {
     
-    var edge_ij_bin = edge_i_bin.filterMetadata('year', 'equals', year_j).mosaic();
+    var edge_ij_bin = edge_i_bin.select('edge_' + year_j);
     
       ages_i = ages_i.addBands(edge_ij_bin)
-      .rename('edge_age_' + year_j)
+      .rename('age_' + year_j)
       .unmask(0);
       
      // if year is not the first, perform age accumulation 
   } else {
     
-    var edge_ij_bin = edge_i_bin.filterMetadata('year', 'equals', year_j).mosaic();
-    var age_past = ages_i.select('edge_age_' + String(year_j - 1));
+    var edge_ij_bin = edge_i_bin.select('edge_' + year_j);
+    var age_past = ages_i.select('age_' + String(year_j - 1));
     
     // perform sum 
     var age_ij = ee.Image(0).where(edge_ij_bin.eq(0).and(age_past.eq(0)), 0)
                             .where(edge_ij_bin.eq(0).and(age_past.gte(1)), 0)
                             .where(edge_ij_bin.eq(1).and(age_past.eq(0)), edge_ij_bin)
                             .where(edge_ij_bin.eq(1).and(age_past.gte(1)), age_past.add(edge_ij_bin))
-                            .rename('edge_age_' + year_j);
+                            .rename('age_' + year_j);
     
     // store
     ages_i = ages_i.addBands(age_ij);
@@ -58,31 +56,22 @@ years.forEach(function(year_j) {
 
 ages_i = ages_i.selfMask();
 print(ages_i);
-Map.addLayer(ages_i.select('edge_age_2024'), {palette: ['green', 'yellow', 'red'], min: 1, max: 35}, 'edge age 2024');
+Map.addLayer(ages_i.select('age_2024'), {palette: ['green', 'yellow', 'red'], min: 1, max: 35}, 'edge age 2024');
 Map.addLayer(ages_i, { }, 'time-series', false);
 
+// insert metadata
+ages_i = ages_i.set('territory', 'BRAZIL')
+              .set('collection_id', collectionId)
+              .set('version', version_out)
+              .set('description', 'EDGE AGE');
+
 // export
-years.forEach(function(year_i) {
-  
-  // select image to export
-  var toExport = ages_i.select('edge_age_' + year_i)
-    .rename('edge_age_' + year_i)
-    .set('territory', 'BRAZIL')
-    .set('collection_id', collectionId)
-    .set('version', version_out)
-    .set('year', year_i)
-    .set('description', 'EDGE AGE');
-    
-  // Edge area
-  Export.image.toAsset({
-  	image: toExport.int8(),
-    description: 'EDGE-AGE' + '-' + year_i + '-' + version_out,
-    assetId: 'projects/mapbiomas-brazil/assets/DEGRADATION/COLLECTION-10/edge-area-age' + '/' + 'EDGE-AGE' + '-' + year_i + '-' + version_out,
-    region: edge_i_bin.geometry(),
-    scale: 30,
-    maxPixels: 1e13,
-    priority: 999
-  });
-})
-
-
+Export.image.toAsset({
+	image: ages_i.int8(),
+  description: fileName,
+  assetId: 'projects/mapbiomas-brazil/assets/DEGRADATION/COLLECTION-10/public/' + fileName,
+  region: edge_i_bin.geometry(),
+  scale: 30,
+  maxPixels: 1e13,
+  priority: 999
+});
